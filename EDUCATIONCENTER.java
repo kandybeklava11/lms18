d6b60734fdeb0c7fae4483974abae0c39e94e273
kandybeklava11 <140234020+kandybeklava11@users.noreply.github.com>
2023-08-18 18:17:14 +0600

    Update EDUCATIONCENTER.java

--- EDUCATIONCENTER.java
+++ EDUCATIONCENTER.java
@@ -2,52 +2,7 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public abstract class EDUCATIONCENTER {
-    /*
-    private String name;
-    private String LocatedCountry;
-    private LocalDate LocadateFoundAtionYear;
-
-    public EDUCATIONCENTER(String name, String locatedCountry, LocalDate locadateFoundAtionYear) {
-        this.name = name;
-        LocatedCountry = locatedCountry;
-        LocadateFoundAtionYear = locadateFoundAtionYear;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLocatedCountry() {
-        return LocatedCountry;
-    }
-
-    public void setLocatedCountry(String locatedCountry) {
-        LocatedCountry = locatedCountry;
-    }
-
-    public LocalDate getLocadateFoundAtionYear() {
-        return LocadateFoundAtionYear;
-    }
-
-    public void setLocadateFoundAtionYear(LocalDate locadateFoundAtionYear) {
-        LocadateFoundAtionYear = locadateFoundAtionYear;
-    }
-     public abstract String getEducationType();
-
-    @Override
-    public String toString() {
-        return "EDUCATIONCENTER{" +
-                "name='" + name + '\'' +
-                ", LocatedCountry='" + LocatedCountry + '\'' +
-                ", LocadateFoundAtionYear=" + LocadateFoundAtionYear +
-                '}';
-    }
-
-     */
+  
         private String name;
         private String country;
         private LocalDate localDate;
